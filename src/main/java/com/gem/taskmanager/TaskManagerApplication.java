5cc2fe1f60465da1f2f64244bc07383be243d535
morfidon <armiksos@gmail.com>
2025-02-25 15:40:56 +0100

    Update TaskManagerApplication.java

--- src/main/java/com/gem/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/gem/taskmanager/TaskManagerApplication.java
@@ -2,12 +2,18 @@ package com.gem.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
-
+    @GetMapping("/home")
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
