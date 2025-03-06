559584bb2c0c5d5f2e59a3c7971321a7b6a67151
morfidon <armiksos@gmail.com>
2025-03-06 12:45:14 +0100

    Creating controllers in seperate files

--- src/main/java/com/gem/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/gem/taskmanager/TaskManagerApplication.java
@@ -2,18 +2,13 @@ package com.gem.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
-    @GetMapping("/home")
-    public String helloWorld(){
-        return "Hello World";
-    }
+
 }
---
+++ src/main/java/com/gem/taskmanager/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.gem.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController
+{
+    @GetMapping("/users")
+    public String getUsers(){
+        return "Here is a list of users";
+    }
+}
