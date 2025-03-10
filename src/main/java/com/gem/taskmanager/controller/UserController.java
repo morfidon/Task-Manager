9bb4b217af90fcaa887d6140af42662ccc482f39
morfidon <armiksos@gmail.com>
2025-03-10 14:01:56 +0100

    Set up UserRepository integration in UserController
    Replaced hardcoded user data with dynamic data retrieval using UserRepository. Annotated the User model with @Table to map it to the "users" database table. Prepared the controller for database-backed user management.

--- src/main/java/com/gem/taskmanager/controller/UserController.java
+++ src/main/java/com/gem/taskmanager/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.gem.taskmanager.controller;
 
 import com.gem.taskmanager.model.User;
+import com.gem.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +12,17 @@ import java.util.List;
 @RestController
 public class UserController
 {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
     @GetMapping("/users")
     public List<User> getUsers()
     {
-        List<User> users = new ArrayList<>();
-        users.add(new User(1, "John", "<EMAIL>", "123456"));
-        users.add(new User(2, "Jane", "<EMAIL>", "123456"));
-        users.add(new User(3, "Jill", "<EMAIL>", "123456"));
-        users.add(new User(4, "Jack", "<EMAIL>", "123456"));
-        return users;
+        return userRepository.findAll();
     }
 }
 //MODEL - SHAPE, FORMULA - DATA - MVC - Model View Controller - VIEW - REACT - VUE -
--- src/main/java/com/gem/taskmanager/model/User.java
+++ src/main/java/com/gem/taskmanager/model/User.java
@@ -3,7 +3,7 @@ package com.gem.taskmanager.model;
 import jakarta.persistence.*;
 
 @Entity
-//@Table(name = "users")
+@Table(name = "users")
 public class User
 {
     @Id
