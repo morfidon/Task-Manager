2eb81ff4de54cf084ccbbbce94e4007dd98e8375
morfidon <armiksos@gmail.com>
2025-03-10 11:37:23 +0100

    Update getUsers endpoint to return a list of User objects
    Changed the getUsers method in UserController to return a list of User objects instead of a static string. This improves the functionality by providing structured data for clients consuming the API.

--- src/main/java/com/gem/taskmanager/controller/UserController.java
+++ src/main/java/com/gem/taskmanager/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.gem.taskmanager.controller;
 
+import com.gem.taskmanager.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController
 {
     @GetMapping("/users")
-    public String getUsers(){
-        return "Here is a list of users of our application";
+    public List<User> getUsers()
+    {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "John", "<EMAIL>", "123456"));
+        users.add(new User(2, "Jane", "<EMAIL>", "123456"));
+        users.add(new User(3, "Jill", "<EMAIL>", "123456"));
+        users.add(new User(4, "Jack", "<EMAIL>", "123456"));
+        return users;
     }
 }
 //MODEL - SHAPE, FORMULA - DATA - MVC - Model View Controller - VIEW - REACT - VUE -
+// JSON
