97c18fc3e1fd5a8d8e75821486d0f93e46121658
morfidon <armiksos@gmail.com>
2025-03-11 13:29:34 +0100

    Simplify user endpoint paths in UserController
    Refactored endpoint mappings in UserController by removing hardcoded path suffixes. Both GET and POST mappings now rely on the base 'users/' path defined at the class level. This improves maintainability and reduces redundancy in endpoint definitions.

--- src/main/java/com/gem/taskmanager/controller/UserController.java
+++ src/main/java/com/gem/taskmanager/controller/UserController.java
@@ -3,15 +3,13 @@ package com.gem.taskmanager.controller;
 import com.gem.taskmanager.model.User;
 import com.gem.taskmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("users/")
 public class UserController
 {
 
@@ -21,16 +19,17 @@ public class UserController
     {
         this.userRepository = userRepository;
     }
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers()
     {
         return userRepository.findAll();
     }
-    @PostMapping("/users")
+    @PostMapping
     public User addUser(@RequestBody User user)
     {
         return userRepository.save(user);
     }
+
 
 }
 // HTTP REQUEST - ONE OF THE TYPE - GET
