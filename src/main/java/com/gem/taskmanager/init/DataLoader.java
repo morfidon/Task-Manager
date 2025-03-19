456bb579dc20737e5f1b49d3c017b02cc964a38e
morfidon <armiksos@gmail.com>
2025-03-19 14:07:53 +0100

    Add sample tasks to DataLoader initialization
    Extended the DataLoader to initialize sample tasks alongside users. Introduced TaskRepository dependency and added sample task creation for both admin and regular users. This ensures a more comprehensive dataset for testing and development.

--- src/main/java/com/gem/taskmanager/init/DataLoader.java
+++ src/main/java/com/gem/taskmanager/init/DataLoader.java
@@ -1,7 +1,9 @@
 package com.gem.taskmanager.init;
 
 import com.gem.taskmanager.model.Role;
+import com.gem.taskmanager.model.Task;
 import com.gem.taskmanager.model.User;
+import com.gem.taskmanager.repository.TaskRepository;
 import com.gem.taskmanager.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +18,13 @@ public class DataLoader implements CommandLineRunner
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
-    public DataLoader(PasswordEncoder passwordEncoder, UserRepository userRepository)
+    public DataLoader(PasswordEncoder passwordEncoder, UserRepository userRepository, TaskRepository taskRepository)
     {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.taskRepository = taskRepository;
     }
     @Override
     public void run(String... args) throws Exception {
@@ -30,6 +34,17 @@ public class DataLoader implements CommandLineRunner
 
             userRepository.saveAll(List.of(admin, user));
             System.out.println( "Loaded Sample Users");
+
+            Task adminTask = new Task("Admin Task", "Admin Task Description", admin);
+            Task adminTask2 = new Task("Admin Task 2", "Admin Task 2 Description", admin);
+
+            Task userTask = new Task("User Task", "User Task Description", user);
+            Task userTask2 = new Task("User Task 2", "User Task 2 Description", user);
+            Task userTask3 = new Task("User Task 3", "User Task 3 Description", user);
+
+            taskRepository.saveAll(List.of(adminTask, adminTask2, userTask, userTask2, userTask3));
+
+            System.out.println( "Loaded Sample Tasks");
         }
 
     }
